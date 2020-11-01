bf1febf636b7b5f7f778a676ae92fb7cfd0d160a
Tien Nguyen <monkiradgbn01@gmail.com>
2020-11-01 16:36:35 +0700

    update search.py

--- search.py
+++ search.py
@@ -2,12 +2,17 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+import pickle
 data_file = 'Data/data_new.csv'
+pickle_file = 'tfidf.pkl'
+X_tfidf_file = 'X_tfidf.pkl'
 data = pd.read_csv(data_file)
 recommend = data[['Titles','key_notes']]
-vectorizer = TfidfVectorizer()
-X_tfidf = vectorizer.fit_transform(recommend['key_notes'])
+with open(pickle_file, 'rb') as file:
+    vectorizer = pickle.load(file)
+with open(X_tfidf_file, 'rb') as file:
+    X_tfidf = pickle.load(file)
+
 
 def recommend_movie(query):
     Y_tfidf = vectorizer.transform([query])
